20cebc5d11dbef8655310473d0be052cf6ba629f
j-mendez <jeff@a11ywatch.com>
2023-11-01 07:42:08 -0400

    chore(audit): cleanup audit entry

--- accessibility-rs/src/engine/audit/wcag.rs
+++ accessibility-rs/src/engine/audit/wcag.rs
@@ -4,11 +4,8 @@ use crate::engine::rules::wcag_rule_map::RULES_A;
 use crate::i18n::locales::get_message_i18n;
 use crate::Auditor;
 
-/// baseline for all rules
-#[derive(Default)]
-pub struct WCAGAAA;
-
 /// validate rule and push issue
+#[inline]
 fn push_issue(
     validation: Validation,
     rule: &Rule,
@@ -16,16 +13,13 @@ fn push_issue(
     lang: &str,
     issues: &mut Vec<Issue>,
 ) {
-    let message = validation.message;
-
     if !validation.valid {
-        let message = if !message.is_empty() {
-            message.into()
-        } else {
-            get_message_i18n(&rule, &validation.id, &lang)
-        };
         issues.push(Issue::new(
-            message,
+            if !validation.message.is_empty() {
+                validation.message.into()
+            } else {
+                get_message_i18n(&rule, &validation.id, &lang)
+            },
             &context,
             &[
                 "WCAGAAA",
@@ -40,21 +34,22 @@ fn push_issue(
     }
 }
 
+/// baseline for all rules
+#[derive(Default)]
+pub struct WCAGAAA;
+
 /// wcag rules to test for
 impl WCAGAAA {
     /// audit html against WCAGAAA standards
     pub fn audit(auditor: &Auditor<'_>) -> Vec<Issue> {
-        // TODO: push rules found to MAP that are different across nodes to combine the selectors Ex: <a> and <link> both have the href attribute.
         let mut issues: Vec<Issue> = Vec::new();
 
         for node in &auditor.tree {
             if RULES_A.contains_key(&*node.0) {
-                let rules = RULES_A.get(&*node.0);
-                match rules {
+                match RULES_A.get(&*node.0) {
                     Some(rules) => {
                         for rule in rules {
-                            let validation = (rule.validate)(&node.1, &auditor.locale);
-                            match validation {
+                            match (rule.validate)(&node.1, &auditor.locale) {
                                 RuleValidation::Single(validation) => push_issue(
                                     validation,
                                     rule,
