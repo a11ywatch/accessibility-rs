ee91f91769df7191de157c3a7216a51cf0b3f11a
j-mendez <jeff@a11ywatch.com>
2024-02-28 12:27:16 -0500

    test(contrast): add blue on blue case

--- accessibility-rs/tests/unit/contrast.rs
+++ accessibility-rs/tests/unit/contrast.rs
@@ -7,7 +7,9 @@ use maud::html;
 /// anchor has valid contrast.
 fn _audit_contrast_text_anchor() {
     let markup = html! {
-        a href="www.example.com";
+        body style="background: blue;" {
+            a href="www.example.com" style="color:blue;" { "example" };
+        }
     };
     let audit = accessibility_rs::audit(AuditConfig::basic(&markup.into_string()));
     let valid = !audit
